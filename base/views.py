54a8d8c174db184dadc235554f8785e2bdbdc644
SuruBasnet <surubasnet824@gmail.com>
2024-02-07 07:44:44 +0545

    initial commit

---
+++ base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Todo
+
+# Create your views here.
+def home(request):
+    todo_objs = Todo.objects.all()
+    data = {'todos':todo_objs}
+    return render(request,'index.html',context=data)
